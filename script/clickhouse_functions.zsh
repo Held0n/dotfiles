20dd49552103c1e856ec63f886ba9e303b2470e3
held0n <764165887@qq.com>
2023-04-14 11:50:16 +0800

    2023-04-14 11:50

--- script/clickhouse_functions.zsh
+++ script/clickhouse_functions.zsh
@@ -1,8 +1,21 @@
 #!/bin/zsh
 
 function kill_clickhouse() {
-    ps -ef | grep clickhouse | grep -v grep | awk '{print $2}' | xargs kill -9
+    if [[ $(ps -ef | grep clickhouse | grep -v grep | awk '{print $2}') ]]; then
+        ps -ef | grep clickhouse | grep -v grep | awk '{print $2}' | xargs kill -9
+    fi
+}
+
+function redkv-cli() {
+    local index=$1
+    [[ $1 ]] || index=0;
+    host=$(cat /data/clickhouse/kv_endpoints.json | jq -r ".endpoints[$index].address" | cut -d : -f 1)
+    port=$(cat /data/clickhouse/kv_endpoints.json | jq -r ".endpoints[$index].address" | cut -d : -f 2)
+    redis-cli -h $host -p $port
 }
 
 function start_metastore() {
+    local metastore_home="/root/project/clickhouse-metastore"
+    cd $metastore_home
+    mvn clean compile && mvn spring-boot:run &
 }
